fee0ae150a3666f3d42e570b3c919135ce051f3e
Harsh <harshvaghasiya21@gmail.com>
2023-04-08 17:05:34 +0530

    Code run without dataclass function

--- drive_detect.py
+++ drive_detect.py
@@ -5,21 +5,28 @@ from dataclasses import dataclass
 from typing import Callable, List
 
 # !Data Class function
+# @dataclass
+# class Drive:
+#     letter: str
+#     disk_name: str
+#     drive_type: str
 
 
-@dataclass
-class Drive:
-    letter: str
-    disk_name: str
-    drive_type: str
-
-
-@property
-def is_removable(self) -> bool:
-    return self.drive_type == 'Removable Disk'
+# @property
+# def is_removable(self) -> bool:
+#     return self.drive_type == 'Removable Disk'
 
 # ! continuels run this program
 # subprocess.Popen(["python", "new.py"], stdout=subprocess.PIPE)
+
+# !  This is code for sencond method of run code continues
+# def watch_drives(on_change: Callable[[List[Drive]], None], poll_interval: int = 1):
+#     prev = None
+#     while True:
+#         drives = list_drives()#         if prev != drives:
+#             on_change(drives)
+#             prev = drives
+#         sleep(poll_interval)
 
 
 def list_drives():
@@ -33,8 +40,24 @@ def list_drives():
         text=True,
         stdout=subprocess.PIPE
     )
+
+    # ? Powershell code to detect the all the usb devices
 #  Get-pnpDevice -Class USB |  Where-Object { $_.InstanceId -match '^USB' } | Select-object FriendlyName  |ConvertTo-Json
 
+    devices = json.loads(proc.stdout)
+
+    return devices
+
+# ? Return using dataclass
+    # return [Drive(
+    #     letter=d['deviceid'],
+    #     disk_name=d['volumename'],
+    #     drive_type=drive_types[d['drivetype']]
+    # )for d in devices]
+
+
+# Function to cheak new drives are inserted or not
+def watch_drives():
     drive_types = {
         0: 'Unknown',
         1: 'No Root Directory',
@@ -44,30 +67,20 @@ def list_drives():
         5: 'Compact Disc',
         6: 'RAM Disk',
     }
-
-    devices = json.loads(proc.stdout)
-    # for d in devices:
-    #     print("Drive_Letter :- {0} Drive_name :- {1} Drive_Type :- {2}".format(
-    #         d['deviceid'], d['volumename'], drive_types[d['drivetype']]), sep='\n')
-        
-    return[Drive(
-            letter=d['deviceid'],
-            disk_name=d['volumename'],
-            drive_type=drive_types[d['drivetype']]
-
-        )for d in devices]
-
-
-# Function to cheak new drives are inserted or not
-def watch_drives(on_change: Callable[[List[Drive]], None], poll_interval: int = 1):
     prev = None
     while True:
         drives = list_drives()
+
         if prev != drives:
-            on_change(drives)
-            prev = drives
-        sleep(poll_interval)
+            for d in drives:
+                print("Drive_Letter :- {0} Drive_name :- {1} Drive_Type :- {2}".format(
+                    d['deviceid'], d['volumename'], drive_types[d['drivetype']]), sep='\n')
+            print('\n')    
+
+        prev = drives
+
 
         # Main function
 if __name__ == '__main__':
-    watch_drives(on_change=print)
+    # Calling the continues cheak function
+    watch_drives()
