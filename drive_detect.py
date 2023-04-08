a40c47fd3f10d1347361839540ae0d742f303904
Harsh <harshvaghasiya21@gmail.com>
2023-04-08 16:02:45 +0530

    device detect loop on

--- drive_detect.py
+++ drive_detect.py
@@ -5,14 +5,18 @@ from dataclasses import dataclass
 from typing import Callable, List
 
 # !Data Class function
+
+
 @dataclass
-class drive:
-    letter:str
-    disk_name:str
-    drive_type:str
+class Drive:
+    letter: str
+    disk_name: str
+    drive_type: str
+
+
 @property
-def is_removable(self)->bool:
-    return self.drive_type=='Removable Disk'
+def is_removable(self) -> bool:
+    return self.drive_type == 'Removable Disk'
 
 # ! continuels run this program
 # subprocess.Popen(["python", "new.py"], stdout=subprocess.PIPE)
@@ -31,8 +35,6 @@ def list_drives():
     )
 #  Get-pnpDevice -Class USB |  Where-Object { $_.InstanceId -match '^USB' } | Select-object FriendlyName  |ConvertTo-Json
 
-
-
     drive_types = {
         0: 'Unknown',
         1: 'No Root Directory',
@@ -47,7 +49,25 @@ def list_drives():
     for d in devices:
         print("Drive_Letter :- {0} Drive_name :- {1} Drive_Type :- {2}".format(
             d['deviceid'], d['volumename'], drive_types[d['drivetype']]), sep='\n')
+        
+        return[Drive(
+            letter=d['deviceid'],
+            disk_name=d['volumename'],
+            drive_type=drive_types[d['drivetype']]
+
+        )for d in devices]
 
 
+# Function to cheak new drives are inserted or not
+def watch_drives(on_change: Callable[[List[Drive]], None], poll_interval: int = 1):
+    prev = None
+    while True:
+        drives = list_drives()
+        if prev != drives:
+            on_change(drives)
+            prev = drives
+        sleep(poll_interval)
+
+        # Main function
 if __name__ == '__main__':
-    list_drives()
+    watch_drives(on_change=print)
